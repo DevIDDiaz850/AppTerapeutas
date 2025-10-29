cf8dc31ac8e12e61bac83029c2c17aa31fcc9f97
deivid0505 <1120209@unav.edu.mx>
2025-10-29 13:10:48 -0700

    activity

---
+++ src/screens/Activity.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function Activity (){
+
+    return(
+        <View>
+            <Text>
+                hola amigous
+            </Text>
+        </View>
+    )
+
+}
+export default Activity
