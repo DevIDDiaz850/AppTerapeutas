baad381f94fdd1c1ddd403ddfe4d2d6d89d711f6
Luis David Diaz Hoil <160052930+DevIDDiaz850@users.noreply.github.com>
2025-10-29 13:44:03 -0700

    Add files via upload

---
+++ src/screens/VideosThoughts.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+function VideoThoughts (){
+    return(
+        <View>
+            <Text> video</Text>
+        </View>
+    )
+}
+
+export default VideoThoughts
